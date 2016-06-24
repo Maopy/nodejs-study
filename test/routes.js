d6e06924b999c7f72bfdd98304cc0708734b366a
Maopy <maopy@51haoyayi.com>
2016-06-24 09:06:24 +0800

    superkoa test

--- test/routes.js
+++ test/routes.js
@@ -1,15 +1,49 @@
 'use strict'
 
 import test from 'ava'
-import fetch from 'node-fetch'
+import request from 'supertest'
 
-import app from '../src/server/app'
+const superkoa = (appPath) => {
+  let _path = appPath !== undefined ? appPath : '../../app.js'
+  let koa = require(_path)
+  return request(koa.listen())
+}
 
-const localhost = 'http://localhost:4000'
-
-test('open index page', async (t) => {
-  await fetch(`${localhost}/`)
-    .then((res) => {
-      t.is(res.status, 200)
-    })
+test('reg success', async (t) => {
+  let regUser = {
+    username: 'Maopy',
+    password: '123456'
+  }
+  let res = await superkoa('../src/server/app.js')
+    .post('/reg')
+    .send(regUser)
+  let ret = res.body
+  t.is(ret.status, 0)
+  t.true('username' in ret.data)
+  t.true('password' in ret.data)
 })
+
+// test('login success', async (t) => {
+//   let loginUser = {
+//     username: 'Maopy2',
+//     password: '123456'
+//   }
+//   return await fetch(`${localhost}/login`, {
+//     method: 'POST',
+//     body: loginUser 
+//   })
+//     .then((res) => {
+//       t.true(res.ok)
+//       t.is(res.status, 200)
+//       return res.json()
+//     })
+//     .then((ret) => {
+//       console.log(ret, 1)
+//       t.is(ret.status, 0)
+//       t.true('username' in ret.data)
+//       t.true('password' in ret.data)
+//     })
+//     .then(() => {
+//       exit(1)
+//     })
+// })
